5f897bca47cdbc3710d391c0fae4903914db4871
Agustin Camino <agustin.camino@gmail.com>
2015-07-01 09:28:53 -0500

    add migrations

---
+++ AppointmentReminders.Web/Migrations/201507011415152_InitialCreate.cs
@@ -0,0 +1,29 @@
+namespace AppointmentReminders.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Appointments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        PhoneNumber = c.String(nullable: false),
+                        Time = c.DateTime(nullable: false),
+                        CreatedAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Appointments");
+        }
+    }
+}
---
+++ AppointmentReminders.Web/Models/AppointmentRemindersContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AppointmentReminders.Web.Models
+{
+    public class AppointmentRemindersContext : DbContext
+    {
+        public AppointmentRemindersContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
