944e9f9cd16948746cb4b76ae05ca9a35d8f8517
Agustin Camino <agustin.camino@gmail.com>
2015-07-01 08:55:12 -0500

    appointment reminders example in ASP.NET MVC

---
+++ AppointmentReminders.Web/Controllers/AppointmentsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AppointmentReminders.Web.Models;
+
+namespace AppointmentReminders.Web.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        // GET: Appointments
+
+        public ActionResult Index()
+        {
+            var appointments = new List<Appointment>
+            {
+               new Appointment{Id = 1, Name = "Frank", PhoneNumber = "+593992670240",Time = new DateTime(), CreatedAt = new DateTime() } 
+            };
+            return View(appointments);
+
+        }
+
+        // GET: Appointments/Create
+
+        public ActionResult Create()
+        {
+
+            return View();
+
+        }
+
+        [HttpPost]
+
+        public ActionResult Create(FormCollection collection)
+        {
+
+            return View();
+        }
+
+        // GET: Appointments/Edit/5
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            // Find participant
+
+            return View(new Appointment());
+
+        }
+    }
+}
---
+++ AppointmentReminders.Web/Models/Appointment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mime;
+using System.Web;
+
+namespace AppointmentReminders.Web.Models
+{
+    public class Appointment
+    {
+        // Appointment.Remindertime
+        public static int ReminderTime = 30;
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Phone number")]
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public DateTime Time { get; set; }
+
+        [Display(Name = "Created at")]
+        public DateTime CreatedAt { get; set; }
+
+        // TODO: Add callback to setup reminder
+        // This processing should be done asynchronously
+    }
+}
