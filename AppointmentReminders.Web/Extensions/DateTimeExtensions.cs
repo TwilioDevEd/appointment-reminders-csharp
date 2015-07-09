3762df89cc7f1938dbba1ccc402849558551b344
Agustin Camino <agustin.camino@gmail.com>
2015-07-09 16:47:12 -0500

    add datetime to local time extension

---
+++ AppointmentReminders.Web/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppointmentReminders.Web.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime ToLocalTime(this DateTime? time, string timezone)
+        {
+            return TimeZoneInfo.ConvertTimeToUtc(
+                time.Value,
+                TimeZoneInfo.FindSystemTimeZoneById(timezone))
+                .ToLocalTime();
+        }
+    }
+}
