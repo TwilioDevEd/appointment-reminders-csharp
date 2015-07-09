dae5dcb9527680ecc2ad0c4d9c9f68dae9c9aa1b
Agustin Camino <agustin.camino@gmail.com>
2015-07-09 10:50:40 -0500

    change phone validation annotation on appointment

--- AppointmentReminders.Web/Models/Appointment.cs
+++ AppointmentReminders.Web/Models/Appointment.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.Ajax.Utilities;
 
 namespace AppointmentReminders.Web.Models
 {
     public class Appointment
     {
-        // Appointment.Remindertime
         public static int ReminderTime = 30;
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
-        [Display(Name = "Phone number")]
-        [Required]
-        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "The Phone number is not valid.")]
+
+        [Required, Phone, Display(Name = "Phone number")]
         public string PhoneNumber { get; set; }
+
         [Required]
         public DateTime? Time { get; set; }
 
@@ -22,8 +23,5 @@ namespace AppointmentReminders.Web.Models
 
         [Display(Name = "Created at")]
         public DateTime CreatedAt { get; set; }
-
-        // TODO: Add callback to setup reminder
-        // This processing should be done asynchronously
     }
 }
