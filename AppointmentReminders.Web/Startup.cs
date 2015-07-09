4cad4b8fb76efed41fbb790894b3dced0735644f
Agustin Camino <agustin.camino@gmail.com>
2015-07-09 10:24:05 -0500

    add hangfire

---
+++ AppointmentReminders.Web/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Hangfire;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AppointmentReminders.Web.Startup))]
+
+namespace AppointmentReminders.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration
+                .UseSqlServerStorage("AppointmentReminders");
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
