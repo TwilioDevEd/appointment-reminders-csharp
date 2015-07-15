605663303796f78cedca0178075fff3d8c7e9cb9
Agustin Camino <agustin.camino@gmail.com>
2015-07-15 14:51:23 -0500

    fix format date on appointment creation

---
+++ AppointmentReminders.Web/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+
+namespace AppointmentReminders.Web.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToCustomDateString(this DateTime dateTime)
+        {
+            return dateTime.ToString("MM/dd/yyyy hh:mm tt");
+        }
+    }
+}
---
+++ Appointments.Web.Tests/Extensions/DateTimeExtensionsTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using AppointmentReminders.Web.Extensions;
+
+
+namespace Appointments.Web.Tests.Extensions
+{
+    public class DateTimeExtensionsTest
+    {
+        [Test]
+        public void ToCustomDateString_returns_a_given_date_in_a_custom_format()
+        {
+            var date = new DateTime(2015, 07, 15, 14, 14, 00);
+            var result = date.ToCustomDateString();
+            Assert.That(result, Is.EqualTo("07/15/2015 02:14 PM"));
+        }
+    }
+}
