30fc152ed636b4753252ae9c1ec1e05180cc7eda
Agustin Camino <agustin.camino@gmail.com>
2015-07-16 14:04:35 -0500

    refactor SendNotificationJob by extracting RestClient into its own class

--- AppointmentReminders.Web/Workers/SendNotificationsJob.cs
+++ AppointmentReminders.Web/Workers/SendNotificationsJob.cs
@@ -1,46 +1,31 @@
 ﻿using System;
-using System.Web.Configuration;
+using System.Collections.Generic;
 using AppointmentReminders.Web.Domain;
+using AppointmentReminders.Web.Models;
 using AppointmentReminders.Web.Models.Repository;
-using Twilio;
+using WebGrease.Css.Extensions;
 
 namespace AppointmentReminders.Web.Workers
 {
     public class SendNotificationsJob
     {
-        public TwilioRestClient TwilioClient
-        {
-            get
-            {
-                var accountSid = WebConfigurationManager.AppSettings["AccountSid"];
-                var authToken = WebConfigurationManager.AppSettings["AuthToken"];
-
-                return new TwilioRestClient(accountSid, authToken);
-            }
-        }
+        private const string MessageTemplate =
+            "Hi {0}. Just a reminder that you have an appointment coming up at {1}.";
 
         public void Execute()
         {
-            const string messageTemplate =
-                "Hi {0}. Just a reminder that you have an appointment coming up at {1}.";
+            var twilioRestClient = new Domain.Twilio.RestClient();
 
-            var appointmentsFinder = new AppointmentsFinder(new AppointmentRepository(), new TimeConverter());
-            var availableAppointments = appointmentsFinder.FindAvailableAppointments(DateTime.Now);
-
-            if (availableAppointments.Count == 0)
-            {
-                return;
-            }
-
-            var twilioNumber = WebConfigurationManager.AppSettings["TwilioNumber"];
-
-            foreach (var appointment in availableAppointments)
-            {
-                TwilioClient.SendSmsMessage(
-                twilioNumber,
+            AvailableAppointments().ForEach(appointment =>
+                twilioRestClient.SendMessage(
                 appointment.PhoneNumber,
-                string.Format(messageTemplate, appointment.Name, appointment.Time.ToString("t")));
-            }
+                string.Format(MessageTemplate, appointment.Name, appointment.Time.ToString("t"))));
+        }
+
+        private static IEnumerable<Appointment> AvailableAppointments()
+        {
+            return new AppointmentsFinder(new AppointmentRepository(), new TimeConverter())
+                .FindAvailableAppointments(DateTime.Now);
         }
     }
 }
